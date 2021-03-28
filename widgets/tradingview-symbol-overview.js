eb88c1f857f4b8dfdfbce78cba5c1b3f217024d1
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-03-28 17:52:35 -0400

    Create tradingview-symbol-overview.js

---
+++ widgets/tradingview-symbol-overview.js
@@ -0,0 +1,39 @@
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_460f8"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/AAPL/" rel="noopener" target="_blank"><span class="blue-text">Apple</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.MediumWidget(
+  {
+  "symbols": [
+    [
+      "Apple",
+      "AAPL"
+    ],
+    [
+      "Google",
+      "GOOGL"
+    ],
+    [
+      "Microsoft",
+      "MSFT"
+    ]
+  ],
+  "chartOnly": false,
+  "width": 1000,
+  "height": 400,
+  "locale": "en",
+  "colorTheme": "light",
+  "gridLineColor": "#F0F3FA",
+  "trendLineColor": "#2196F3",
+  "fontColor": "#787B86",
+  "underLineColor": "#E3F2FD",
+  "isTransparent": false,
+  "autosize": false,
+  "container_id": "tradingview_460f8"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
