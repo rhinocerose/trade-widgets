db0108baf9b35cdd90fcc5cb181e4fb54e520f50
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-03-28 17:48:09 -0400

    Create tradingview-chart.js

---
+++ widgets/tradingview-chart.js
@@ -0,0 +1,34 @@
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_655e2"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "withdateranges": true,
+  "range": "YTD",
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "watchlist": [
+    "COINBASE:BTCUSD",
+    "AMEX:SPY"
+  ],
+  "details": true,
+  "hotlist": true,
+  "calendar": true,
+  "container_id": "tradingview_655e2"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
